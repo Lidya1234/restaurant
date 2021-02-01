14b58d597a80e0d4d7bff726849722051bf83bd5
Lidya1234 <lidya94gher@gmail.com>
2021-02-01 18:27:23 +0300

    Add contact.js file

---
+++ src/clear.js
@@ -0,0 +1,9 @@
+const clear = () =>
+{
+    const content =document.querySelector('#content')
+    while(content.firstChild)
+    {
+        content.removeChild(content.firstChild);
+    }
+}
+export default clear;
